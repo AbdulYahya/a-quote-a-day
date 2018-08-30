158e1229bf167b8eeb0d3eec1e516e82d3ee303b
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2018-08-30 01:00:12 -0700

    Refactored Api call to use new JS fetch API. Removed Axios package

--- assets/scripts/grabQuote.js
+++ assets/scripts/grabQuote.js
@@ -1,16 +1,18 @@
-const axios = require('axios');
-
 const url = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
 
 const quote = document.querySelector('.quote');
 const author = document.querySelector('.author');
 
-axios.get(url)
-  .then(function (response) {
-    let quoteContent = response.data[0].content.replace(/\<p>|<\/p>/g, '');
-    quote.innerHTML = quoteContent;
-    author.innerHTML = `- ${response.data[0].title}`;
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+const fetchQuote = async () => 
+  await (await fetch(url)).json();
+
+fetchQuote()
+  .then(
+    data => { 
+      let quoteContent = data[0].content.replace(/\<p>|<\/p>/g, '');
+      console.log(data[0].title);
+      console.log(quoteContent);
+      author.innerHTML = `- ${data[0].title}`;
+      quote.innerHTML = quoteContent;
+    })
+  .catch(reason => console.log(reason.message));
