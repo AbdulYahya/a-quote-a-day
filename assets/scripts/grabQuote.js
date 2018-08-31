f00b226ace2d5f9236392697a806473c5072ab4f
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2018-08-31 02:52:20 -0700

    Added custom fonts along with separate font stylesheet. Refactored method used to traverse Tipe GraphQL CMS data. Added font loading to webpack. Updated grid structure. Updated overall styling. Removed some styling.

--- assets/scripts/grabQuote.js
+++ assets/scripts/grabQuote.js
@@ -1,16 +1,14 @@
 const url = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
 
-const quote = document.querySelector('.quote');
-const author = document.querySelector('.author');
-
 const fetchQuote = async () => 
   await (await fetch(url)).json();
 
 fetchQuote()
   .then(
     data => { 
+      let wrapper = document.querySelector('.wrapper');
       let quoteContent = data[0].content.replace(/\<p>|<\/p>/g, '');
-      author.innerHTML = `- ${data[0].title}`;
-      quote.innerHTML = quoteContent;
+      wrapper.innerHTML = `<h1 class="quote">${quoteContent}</h1>` +
+                          `<p class="author">- ${data[0].title}</p>`;
     })
   .catch(reason => console.log(reason.message));
